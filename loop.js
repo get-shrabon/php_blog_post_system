d0e446dd41bb70f7af1092516bc91a8c3164b015
Shrabon Talukder <getshrabon@gmail.com>
2024-05-29 20:17:34 +0600

    update

---
+++ loop.js
@@ -0,0 +1,12 @@
+const name = "Tanjim al Hadi";
+
+// console.log(name)
+
+let a = 10;
+let b = 20;
+let result = a * b;
+// console.log(result)
+
+for (let i = 0; i <= 100; i++) {
+console.log(i)
+}

b0c74234734e75075a37b90aaba5a28906bb5bc8
Shrabon Talukder <getshrabon@gmail.com>
2024-05-31 23:45:08 +0600

    Add Some Css

--- dbConnection.php
+++ dbConnection.php
@@ -6,8 +6,3 @@ $hostName = "localhost";
 
 $mysqli = mysqli_connect($hostName, $username, $password, $dbname);
 
-// if (!$mysqli) {
-//     die("Connection Error" . $mysqli);
-// } else {
-//     echo "Connection Successful!";
-// }
